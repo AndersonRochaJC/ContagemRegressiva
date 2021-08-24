44b43f9597b6b408dc4ef59aecaea1e21a7a8c7e
AndersonRochaJC <jmcreggae@gmail.com>
2021-08-24 16:32:02 -0300

    ContagemRegressiva

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+             Console.WriteLine("Preparar...");
+             Console.ReadKey();
+             Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+             Console.WriteLine("Apontar...");
+             Console.ReadKey();
+             Console.BackgroundColor = ConsoleColor.Red;
+                         Console.ForegroundColor = ConsoleColor.Black;
+                          Console.WriteLine("Fogo!!!"); 
+                           Console.ReadKey();                         
+                          Console.WriteLine("POW !!!");
+             Console.ReadKey();
+             Console.WriteLine("POW !!!");
+             Console.ReadKey();
+             Console.WriteLine("POW !!!");
+              Console.WriteLine("Hasta la vista baby !!!");              
+              Console.BackgroundColor = ConsoleColor.Black;
+              Console.ResetColor();
+
+        }
+    }
+}
